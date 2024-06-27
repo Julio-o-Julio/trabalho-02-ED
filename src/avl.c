5115066b61fdf7e78d2948098f19985567010fc0
Julio Souza <julioamsf@gmail.com>
2024-06-27 18:47:16 -0400

    First commit

--- src/avl.c
+++ src/avl.c
@@ -1,110 +1,186 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "./avl.h"
+#include "../include/avl.h"
 
-int max(int a, int b){
-	return a > b ? a : b;
+int max(int a, int b)
+{
+    return a > b ? a : b;
 }
 
-int altura(tnode *arv){
-	if (arv == NULL)
-		return -1;
-	else
-		return arv->h; 
+int altura(tnode *arv)
+{
+    int ret;
+    if (arv == NULL)
+    {
+        ret = -1;
+    }
+    else
+    {
+        ret = arv->h;
+    }
+    return ret;
 }
 
-void _rd(tnode ** parv){
-	tnode * y = *parv;
-	tnode * x = *y->esq;
-	tnode * A = *x->esq;
-	tnode * B = *x->dir;
-	tnode * C = *y->dir;
-
-	y->esq = B;
-	x->dir = y;
-	*parv = x;
-
-	y->h = max(altura(B), altura(C)) + 1;
-	x->h = max(altura(A), altura(y)) + 1;
+void avl_insere(tnode **parv, titem item)
+{
+    if (*parv == NULL)
+    {
+        *parv = (tnode *)malloc(sizeof(tnode));
+        (*parv)->item = item;
+        (*parv)->esq = NULL;
+        (*parv)->dir = NULL;
+        (*parv)->h = 0;
+    }
+    else if (((*parv)->item - item) > 0)
+    {
+        avl_insere(&(*parv)->esq, item);
+    }
+    else
+    {
+        avl_insere(&(*parv)->dir, item);
+    }
+    (*parv)->h = max(altura((*parv)->esq), altura((*parv)->dir)) + 1;
+    _avl_rebalancear(parv);
 }
 
-void _re(tnode ** parv){
-	tnode * x = *parv;
-	tnode * y = x->dir;
-	tnode * A = x->esq;
-	tnode * B = y->esq;
-	tnode * C = y->dir;
+void _rd(tnode **parv)
+{
+    tnode *y = *parv;
+    tnode *x = y->esq;
+    tnode *A = x->esq;
+    tnode *B = x->dir;
+    tnode *C = y->dir;
 
-	x->dir = B;
-	y->esq = x;
-	*parv = y;
-	
-	x->h = max(altura(A), altura(B)) + 1;
-	y->h = max(altura(x), altura(C)) + 1;
+    y->esq = B;
+    x->dir = y;
+    *parv = x;
+    y->h = max(altura(B), altura(C)) + 1;
+    x->h = max(altura(A), altura(y)) + 1;
 }
 
-void _avl_rebalancear(tnode ** parv){
-	int fb;
-	int fbf;
-	tnode * filho;
-	fb = altura((*parv)->esq) - altura((*parv)->dir);
+void _re(tnode **parv)
+{
+    tnode *x = *parv;
+    tnode *y = x->dir;
+    tnode *A = x->esq;
+    tnode *B = y->esq;
+    tnode *C = y->dir;
 
-	if (fb == -2){
-		filho = (*parv)->dir;
-		fbf = altura(filho->esq) - altura(filho->dir);
-		if (fbf <= 0) {
-			_re(parv);
-		} else {
-			_rd(&(*parv)->dir);
-			_re(parv);
-		}
-	} else if (fb == 2) {
-		filho = (*parv)->esq;
-		fbf = altura((*parv)->esq) - altura((*parv)->dir);
-		if (fbf >= 0){
-			_rd(parv);
-		} else {
-			_re(&(*parv)->esq);
-			_rd(parv);
-		}
-	}
+    x->dir = B;
+    y->esq = x;
+    *parv = y;
+    x->h = max(altura(A), altura(B)) + 1;
+    y->h = max(altura(x), altura(C)) + 1;
 }
 
-void insere(tnode ** parv, titem item){
-	if (*parv == NULL){
-		*parv = (tnode *) malloc(sizeof(tnode));
-		(*parv)->item = item;
-		(*parv)->esq = NULL;
-		(*parv)->dir = NULL;
-		(*parv)->h = 0;
-	} else if (((*parv)->item - item) > 0){
-		insere(&(*parv)->esq, item);
-	} else {
-		insere(&(*parv)->dir, item);
-	}
+void _avl_rebalancear(tnode **parv)
+{
+    int fb;
+    int fbf;
+    tnode *filho;
+    fb = altura((*parv)->esq) - altura((*parv)->dir);
 
-	(*parv)->h = max(altura((*parv)->esq), altura((*parv)->dir)) + 1;
-	_avl_rebalancear(parv);
+    if (fb == -2)
+    {
+        filho = (*parv)->dir;
+        fbf = altura(filho->esq) - altura(filho->dir);
+        if (fbf <= 0)
+        { /* Caso 1  --> ->*/
+            _re(parv);
+        }
+        else
+        { /* Caso 2  --> <-*/
+            _rd(&(*parv)->dir);
+            _re(parv);
+        }
+    }
+    else if (fb == 2)
+    {
+        filho = (*parv)->esq;
+        fbf = altura(filho->esq) - altura(filho->dir);
+        if (fbf >= 0)
+        { /* Caso 3  <-- <-*/
+            _rd(parv);
+        }
+        else
+        { /* Caso 4  <-- ->*/
+            _re(&(*parv)->esq);
+            _rd(parv);
+        }
+    }
 }
 
-tnode ** percorre_esq(tnode ** parv){
-	tnode * aux = *parv;
-	if (aux->esq == NULL){
-		return parv;
-	} else {
-		while(aux->esq->esq != NULL) {
-			aux = aux->esq;
-		return &(aux->esq);
+tnode **percorre_esq(tnode **arv)
+{
+    tnode *aux = *arv;
+    if (aux->esq == NULL)
+    {
+        return arv;
+    }
+    else
+    {
+        while (aux->esq->esq != NULL)
+            aux = aux->esq;
+        return &(aux->esq);
+    }
+}
+void avl_remove(tnode **parv, titem reg)
+{
+    int cmp;
+    tnode *aux;
+    tnode **sucessor;
+    if (*parv != NULL)
+    {
+        cmp = (*parv)->item - reg;
+        if (cmp > 0)
+        { /* ir esquerda*/
+            avl_remove(&((*parv)->esq), reg);
+        }
+        else if (cmp < 0)
+        { /*ir direita*/
+            avl_remove(&((*parv)->dir), reg);
+        }
+        else
+        { /* ACHOU  */
+            if ((*parv)->esq == NULL && (*parv)->dir == NULL)
+            { /* no folha */
+                free(*parv);
+                *parv = NULL;
+            }
+            else if ((*parv)->esq == NULL || (*parv)->dir == NULL)
+            { /* tem um filho*/
+                aux = *parv;
+                if ((*parv)->esq == NULL)
+                {
+                    *parv = (*parv)->dir;
+                }
+                else
+                {
+                    *parv = (*parv)->esq;
+                }
+                free(aux);
+            }
+            else
+            { /* tem dois filhos */
+                sucessor = percorre_esq(&(*parv)->dir);
+                (*parv)->item = (*sucessor)->item;
+                avl_remove(&(*parv)->dir, (*sucessor)->item);
+            }
+        }
+        if (*parv != NULL)
+        {
+            (*parv)->h = max(altura((*parv)->esq), altura((*parv)->dir)) + 1;
+            _avl_rebalancear(parv);
+        }
+    }
 }
 
-void avl_remove(tnode ** parv, titem reg) {
-	
-}
-
-void avl_destroi(tnode *parv){
-	if (parv != NULL) {
-		avl_destroi(parv->esq);
-		avl_destroi(parv->dir);
-		free(parv);
-	}
+void avl_destroi(tnode *parv)
+{
+    if (parv != NULL)
+    {
+        avl_destroi(parv->esq);
+        avl_destroi(parv->dir);
+        free(parv);
+    }
 }
